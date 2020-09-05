f377580c8745b550eaa9c52b06d869d6f19e4144
jonghyeok <alb7979s@naver.com>
2020-09-05 15:19:58 +0900

    200905

--- 2019_KAKAO_BLIND_RECRUITMENT/4_무지의먹방라이브.py
+++ 2019_KAKAO_BLIND_RECRUITMENT/4_무지의먹방라이브.py
@@ -1,1 +1,24 @@
-#https://programmers.co.kr/learn/courses/30/lessons/42891
+#애증의 효율성 문제..
+from heapq import*
+def solution(food_times, k):
+    pq=[]
+    lf=len(food_times)
+    for i in range(lf):
+        heappush(pq, (food_times[i], i+1))
+    eated, cnt = 0, 0
+    while pq:
+        temp = (pq[0][0] - eated) * len(pq)
+        if temp <= k:
+            k -= temp
+            eated = pq[0][0]
+            while pq:
+                if pq[0][0] > eated: break
+                heappop(pq)
+        else:
+            temp = []
+            while pq:
+                time, idx = heappop(pq)
+                temp.append((idx, time))
+            temp.sort()
+            return temp[k%len(temp)][0]
+    return -1
