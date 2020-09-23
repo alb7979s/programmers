720b083dcb54a5a6f429a68e0d43d504b2ebae9b
jonghyeok <alb7979s@naver.com>
2020-09-24 06:50:53 +0900

    200924

--- 2019_카카오2차.py
+++ 2019_카카오2차.py
@@ -1,4 +1,4 @@
-#에러뜸 디버깅하기
+#엘베 하나로 돌림
 import requests
 url = 'http://localhost:8000'
 tops = [5, 25, 25]
@@ -34,7 +34,7 @@ def commandQuery(node, cmd):
 
 def p0_simulator():
     user = 'tester'
-    problem = 0               #문제 번호
+    problem = 2               #문제 번호
     count = 1                 #사용할 엘베 수
     top = tops[problem]       #꼭대기층(1층 ~ 꼭대기층)
     ret = start(user, problem, count)
@@ -45,40 +45,39 @@ def p0_simulator():
         #우선 엘베 한 대로 simulation
         temp = oncalls(token)
         if temp['is_end']: break
-        commandList = []
+        commandList = [[]for _ in range(count)]
         for i, elevator in enumerate(temp['elevators']):
             flag=False                      #이 층에서 탈 수 있거나 내릴 수 있는거 확인
             c = Command(i)
             d = dir[i]                      #방향 저장해놓은거 가져옴
             calls = temp['calls']
             floor = elevator['floor']
-            exited = 0
             # 이 층에서 탈 사람이나(call[3]같고 call[4] 방향에 맞으면), 내릴 사람 있으면(passneger[3]이 층과 같으면))
             for passsenger in elevator['passengers']:  # 내릴사람~~?
                 if passsenger['end'] == floor:
                     c.command['command']='EXIT'
                     c.command['call_ids'].append(passsenger['id'])
-                    exited += 1
-            if exited >= 1:
-                commandList.append(c.receive())
+                    flag=True
+            if flag:
+                commandList[i] = c.receive()
                 c = Command(i)
-                flag=True
-            entered = 0
-            for call in calls:  # 타실??
-                if len(elevator['passengers']) + entered - exited > 8: break
-                if call['start'] == floor:
-                    if d == 1:  # 올라가면a
-                        # if call['end'] > floor:
-                        c.command['command'] = 'ENTER'
-                        c.command['call_ids'].append(call['id'])
-                        entered += 1
-                    else:  # 내려가면
-                        # if call['end'] < floor:
-                        c.command['command'] = 'ENTER'
-                        c.command['call_ids'].append(call['id'])
-                        entered += 1
+            if not flag:
+                entered = 0
+                for call in calls:  # 타실??
+                    if len(elevator['passengers']) + entered + 1 > 8: break
+                    if call['start'] == floor:
+                        if d == 1:  # 올라가면a
+                            if call['end'] > floor:
+                                c.command['command'] = 'ENTER'
+                                c.command['call_ids'].append(call['id'])
+                                entered += 1
+                        else:  # 내려가면
+                            if call['end'] < floor:
+                                c.command['command'] = 'ENTER'
+                                c.command['call_ids'].append(call['id'])
+                                entered += 1
             if entered >= 1:
-                commandList.append(c)
+                commandList[i] = c.receive()
                 c = Command(i)
                 flag=True
             #여기서부터 상태 처리
@@ -87,20 +86,21 @@ def p0_simulator():
             status = elevator['status']
             if status == 'STOPPED':
                 #멈춰있는데 내리거나 탈 사람이 있다~ 열어주세요~~
-                if flag: commandList.append(commandQuery(c, 'OPEN'))
+                if flag: commandList[i] = (commandQuery(c, 'OPEN'))
                 # 그러지 못했음 이동
                 else:
-                    if d==1: commandList.append(commandQuery(c, 'UP'))
-                    else: commandList.append(commandQuery(c, 'DOWN'))
+                    if d==1: commandList[i]=(commandQuery(c, 'UP'))
+                    else: commandList[i] = (commandQuery(c, 'DOWN'))
             elif status == 'UPWARD' or status == 'DOWNWARD':    #가던 중에 멈춰야하면
-                if flag: commandList.append(commandQuery(c, 'STOP'))
+                if flag or floor==top or floor==1: commandList[i] = (commandQuery(c, 'STOP'))
                 else:
-                    if d==1: commandList.append(commandQuery(c, 'UP'))
-                    else: commandList.append(commandQuery(c, 'DOWN'))
+                    if d==1: commandList[i] = (commandQuery(c, 'UP'))
+                    else: commandList[i] = (commandQuery(c, 'DOWN'))
             elif status == 'OPENED':    #열려있음!
-                if flag: pass
-                else: commandList.append(commandQuery(c, 'CLOSE'))
+                if flag: continue
+                else: commandList[i] = (commandQuery(c, 'CLOSE'))
             dir[i] = d
+        # print(status, floor, commandList)
         action(token, commandList)
 
 if __name__ == '__main__':
