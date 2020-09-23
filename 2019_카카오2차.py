57424a12c85faaa175685ac462ef05951b29c3ec
jonghyeok <alb7979s@naver.com>
2020-09-23 17:11:05 +0900

    200923

---
+++ 2019_카카오2차.py
@@ -0,0 +1,108 @@
+#에러뜸 디버깅하기
+import requests
+url = 'http://localhost:8000'
+tops = [5, 25, 25]
+class Command:
+    def __init__(self, i):
+        self.command = {}
+        self.command['elevator_id'] = i  # 엘베 번호
+        self.command['command'] = ''     # 명령
+        self.command['call_ids'] = []    # 나가거나 들어갈 번호
+    def receive(self):
+        if self.command['command'] == 'EXIT' or self.command['command']=='ENTER':
+            return self.command
+        del(self.command['call_ids'])
+        return self.command
+
+def start(user, problem, count):
+    uri = url + '/start' + '/' + user + '/' + str(problem) + '/' + str(count)
+    return requests.post(uri).json()
+
+
+def oncalls(token):
+    uri = url + '/oncalls'
+    return requests.get(uri, headers={'X-Auth-Token': token}).json()
+
+
+def action(token, cmds):
+    uri = url + '/action'
+    return requests.post(uri, headers={'X-Auth-Token': token}, json={'commands': cmds}).json()
+
+def commandQuery(node, cmd):
+    node.command['command'] = cmd
+    return node.receive()
+
+def p0_simulator():
+    user = 'tester'
+    problem = 0               #문제 번호
+    count = 1                 #사용할 엘베 수
+    top = tops[problem]       #꼭대기층(1층 ~ 꼭대기층)
+    ret = start(user, problem, count)
+    token = ret['token']
+    print('Token for %s is %s' % (user, token))
+    dir = [1, 1, 1, 1]     #1위로, 0아래로
+    while 1:
+        #우선 엘베 한 대로 simulation
+        temp = oncalls(token)
+        if temp['is_end']: break
+        commandList = []
+        for i, elevator in enumerate(temp['elevators']):
+            flag=False                      #이 층에서 탈 수 있거나 내릴 수 있는거 확인
+            c = Command(i)
+            d = dir[i]                      #방향 저장해놓은거 가져옴
+            calls = temp['calls']
+            floor = elevator['floor']
+            exited = 0
+            # 이 층에서 탈 사람이나(call[3]같고 call[4] 방향에 맞으면), 내릴 사람 있으면(passneger[3]이 층과 같으면))
+            for passsenger in elevator['passengers']:  # 내릴사람~~?
+                if passsenger['end'] == floor:
+                    c.command['command']='EXIT'
+                    c.command['call_ids'].append(passsenger['id'])
+                    exited += 1
+            if exited >= 1:
+                commandList.append(c.receive())
+                c = Command(i)
+                flag=True
+            entered = 0
+            for call in calls:  # 타실??
+                if len(elevator['passengers']) + entered - exited > 8: break
+                if call['start'] == floor:
+                    if d == 1:  # 올라가면a
+                        # if call['end'] > floor:
+                        c.command['command'] = 'ENTER'
+                        c.command['call_ids'].append(call['id'])
+                        entered += 1
+                    else:  # 내려가면
+                        # if call['end'] < floor:
+                        c.command['command'] = 'ENTER'
+                        c.command['call_ids'].append(call['id'])
+                        entered += 1
+            if entered >= 1:
+                commandList.append(c)
+                c = Command(i)
+                flag=True
+            #여기서부터 상태 처리
+            if floor == 1 and d == -1: d = 1
+            if floor == top and d == 1: d = -1
+            status = elevator['status']
+            if status == 'STOPPED':
+                #멈춰있는데 내리거나 탈 사람이 있다~ 열어주세요~~
+                if flag: commandList.append(commandQuery(c, 'OPEN'))
+                # 그러지 못했음 이동
+                else:
+                    if d==1: commandList.append(commandQuery(c, 'UP'))
+                    else: commandList.append(commandQuery(c, 'DOWN'))
+            elif status == 'UPWARD' or status == 'DOWNWARD':    #가던 중에 멈춰야하면
+                if flag: commandList.append(commandQuery(c, 'STOP'))
+                else:
+                    if d==1: commandList.append(commandQuery(c, 'UP'))
+                    else: commandList.append(commandQuery(c, 'DOWN'))
+            elif status == 'OPENED':    #열려있음!
+                if flag: pass
+                else: commandList.append(commandQuery(c, 'CLOSE'))
+            dir[i] = d
+        action(token, commandList)
+
+if __name__ == '__main__':
+    p0_simulator()
+
--- 2020_KAKAO_BLIND_RECRUITMENT/7_블록이동하기.py
+++ 2020_KAKAO_BLIND_RECRUITMENT/7_블록이동하기.py
@@ -1,38 +1,37 @@
 from collections import*
-dd=[(-1, 0), (0, 1), (1, 0), (0, -1)]
+def add(x1, x2, y1, y2, prev):
+    global q, visit
+    if (x1, x2, y1, y2) not in visit:
+        visit[x1, x2, y1, y2] = prev+1
+        q.append((x1, x2, y1, y2))
 def solution(board):
-    ans = 0
-    n=len(board)
+    global q, visit
     visit={}
-    visit[0, 0, 0, 1] = 0
     q=deque()
-    q.append((0, 0, 0, 1, 0))  #x1, y1, x2, y2, d(0이면 가로, 1이면 세로)
+    visit[0,0,0,1] = 0
+    q.append((0,0,0,1))
+    n=len(board)
     while q:
-        x1, y1, x2, y2, d = q.popleft()
-        if (x1 == n-1 and y1 == n-1) or (x2 == n-1 and y2 == n-1): return visit[x1, y1, x2, y2]
-        #그냥 이동
-        for dx, dy in dd:
+        x1, y1, x2, y2 = q.popleft()
+        if (x1==n-1 and y1==n-1) or (x2==n-1 and y2==n-1): return visit[x1, y1, x2, y2]
+        #4방향 이동
+        prev = visit[x1, y1, x2, y2]
+        for dx, dy in [(0,-1), (0,1), (1,0), (-1,0)]:
             nx1, ny1, nx2, ny2 = x1+dx, y1+dy, x2+dx, y2+dy
-            if nx1<0 or ny1<0 or nx2<0 or ny2<0 or nx1>n-1 or ny1>n-1 or nx2>n-1 or ny2>n-1 or board[nx1][ny1] or board[nx2][ny2]: continue
-            if (nx1, ny1, nx2, ny2) not in visit:
-                visit[nx1, ny1, nx2, ny2] = visit[x1, y1, x2, y2] + 1
-                q.append((nx1, ny1, nx2, ny2, d))
-        #회전
-        if not d:       #가로면
-            for dx in [1, -1]:
-                if 0<= x1+dx < n and not board[x1+dx][y1] and not board[x2+dx][y2]:
-                    if (x2+dx, y2, x2, y2) not in visit:
-                        visit[x2+dx, y2, x2, y2] = visit[x1, y1, x2, y2] + 1
-                        q.append((x2+dx, y2, x2, y2, not d))
-                    if (x1, y1, x1+dx, y1) not in visit:
-                        visit[x1, y1, x1+dx, y1] = visit[x1, y1, x2, y2] + 1
-                        q.append((x1, y1, x1+dx, y1, not d))
-        else:
-            for dy in [1, -1]:
-                if 0<= y1+dy < n and not board[x1][y1+dy] and not board[x2][y2+dy]:
-                    if (x1, y1, x1, y1+dy) not in visit:
-                        visit[x1, y1, x1, y1+dy] = visit[x1, y1, x2, y2] + 1
-                        q.append((x1, y1, x1, y1+dy, not d))
-                    if (x2, y2+dy, x2, y2) not in visit:
-                        visit[x2, y2+dy, x2, y2] = visit[x1, y1, x2, y2] + 1
-                        q.append((x2, y2+dy, x2, y2, not d))
+            if nx1<0 or nx2<0 or ny1<0 or ny2<0 or nx1>n-1 or nx2>n-1 or ny1>n-1 or ny2>n-1 or board[nx1][ny1] or board[nx2][ny2]: continue
+            add(nx1, ny1, nx2, ny2, prev)
+        #회전!
+        for d in [1, -1]:
+            if x1==x2: #가로 방향
+                nx = x1+d
+                if nx<0 or nx>n-1 or board[nx][y1] or board[nx][y2]: continue
+                add(nx, y1, x1, y1, prev)
+                add(nx, y2, x2, y2, prev)
+            if y1==y2:
+                ny = y1+d
+                if ny<0 or ny>n-1 or board[x1][ny] or board[x2][ny]: continue
+                add(x1, ny, x1, y1, prev)
+                add(x2, ny, x2, y2, prev)
+    print(visit)
+    return -1
+print(solution([[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]))
